904ae7aa947b6d8991e6fe25139b0539ac1c5117
grapeperson <grapeperson@inbox.ru>
2018-03-13 20:15:16 +0300

    feature/add methods

--- src/index.js
+++ src/index.js
@@ -1,11 +1,52 @@
-// DO WHATEVER YOU WANT HERE
+ // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
-const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+const createEnumerableProperty = propName => propName;
+
+const createNotEnumerableProperty = (propName) => {
+  Object.defineProperty(Object.prototype, propName, {
+    enumerable: false,
+    value: 'value'
+})
+return propName;
+};
+
+const createProtoMagicObject = () => {
+  const obj = function (){};
+  obj.prototype = obj.__proto__;
+  return obj;
+};
+
+let counter=0;
+const incrementor = () => {
+  counter++;
+  incrementor.toString =()=>counter;
+  return incrementor;
+};
+
+let number=0;
+const asyncIncrementor = () => {
+    number++;
+    return number;
+};
+const createIncrementer = () => {
+  let obj=[];
+  obj.value=0;
+  obj.next=()=>{
+    obj.value++;
+    return obj;
+  }
+  return obj;
+  // class Inc{
+  //   constructor(){
+  //     this.value=0;
+  //   }
+  //   next(){
+  //     this.value++;
+  //     return this
+  //   }
+  // }
+  // return new Inc();
+};
 
 // return same argument not earlier than in one second, and not later, than in two
 const returnBackInSecond = () => {};
